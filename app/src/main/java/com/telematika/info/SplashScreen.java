88d872091649c4c70a1d60a029c5c78157a15292
Hlime-dev <halimctwd@gmail.com>
2023-07-06 00:28:52 +0800

    Animasi Splash Screen

--- app/src/main/java/com/telematika/info/SplashScreen.java
+++ app/src/main/java/com/telematika/info/SplashScreen.java
@@ -1,20 +1,17 @@
 package com.telematika.info;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.WindowManager;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -22,12 +19,6 @@ public class SplashScreen extends AppCompatActivity {
                 startActivity(new Intent(SplashScreen.this, MainActivity.class));
                 finish();
             }
-        }, 2000);
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-        }
-
-        getWindow() .getDecorView(). setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }, 3000);
     }
 }
