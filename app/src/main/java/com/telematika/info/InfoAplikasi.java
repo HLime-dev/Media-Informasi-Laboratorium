72d896a8396a46aaed8a97d28d0e1eb1964c201e
Hlime-dev <halimctwd@gmail.com>
2024-08-19 10:34:56 +0800

    signed apk

--- app/src/main/java/com/telematika/info/InfoAplikasi.java
+++ app/src/main/java/com/telematika/info/InfoAplikasi.java
@@ -2,7 +2,9 @@ package com.telematika.info;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class InfoAplikasi extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class InfoAplikasi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_aplikasi2);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 }
